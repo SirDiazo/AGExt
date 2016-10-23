35ecf8b3cd40c7a1121266610093ebb080265a81
SirDiazo <diazo@hotmail.com>
2016-10-23 12:50:45 -0600

    Dynamic Types fix
    ContractConfigurator/Blizzy's toolbar wrapper fix.

--- AGExt/Button.cs
+++ AGExt/Button.cs
@@ -828,9 +828,15 @@ new Func<Vector2, Vector2>((pos) => value.Draw(pos))
 
         internal static Type getType(string name)
         {
-            return AssemblyLoader.loadedAssemblies
-            .SelectMany(a => a.assembly.GetExportedTypes())
-            .SingleOrDefault(t => t.FullName == name);
+            Type type = null;
+            AssemblyLoader.loadedAssemblies.TypeOperation(t =>
+            {
+                if (t.FullName == name)
+                {
+                    type = t;
+                }
+            });
+            return type;
         }
 
         internal static PropertyInfo getProperty(Type type, string name)
